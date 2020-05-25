709ee474eafb507c0de11034fdc2e062210cc76c
Lalit Narayan Mishra <lnm8910@gmail.com>
2020-05-25 17:14:32 -0400

    challange 2&3

--- challanges.js
+++ challanges.js
@@ -26,19 +26,36 @@ var lastdice;
 document.querySelector(".btn-roll").addEventListener("click", function () {
   if (gamePlaying) {
     //1.Random number
-    dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+
     //2. display results
-    var diceDOM = document.querySelector(".dice");
-    diceDOM.style.display = "block";
-    diceDOM.src = "dice-" + dice + ".png";
+    //var diceDOM = document.querySelector(".dice");
+    document.getElementById("dice-1").style.display = "block";
+    document.getElementById("dice-2").style.display = "block";
+
+    document.getElementById("dice-1").src = "dice-" + dice1 + ".png";
+    document.getElementById("dice-2").src = "dice-" + dice2 + ".png";
 
     //3.update the round score if dice didnt roll to 1.
-    if (dice === 6 && lastdice === 6) {
+
+    if (dice1 !== 1 && dice2 !== 1) {
+      //add score
+      roundScore += dice1 + dice2;
+      //roundScore = roundScore + dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      //next player
+      nextPlayer();
+    }
+
+    /**if (dice === 6 && lastdice === 6) {
       //player looses score
       scores[activePlayer] = 0;
-      document.querySelector("#score-" + activePlayer).textContent = '0';
+      document.querySelector("#score-" + activePlayer).textContent = "0";
       nextPlayer();
-
     } else if (dice !== 1) {
       //add score
       roundScore += dice;
@@ -50,8 +67,7 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
       //next player
       nextPlayer();
     }
-    lastdice = dice;
-    
+    lastdice = dice;*/
   }
 });
 
@@ -63,10 +79,22 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     //2. update the UI.
     document.querySelector("#score-" + activePlayer).textContent =
       scores[activePlayer];
+    var input = document.querySelector(".final-score").value;
+    var winningScore;
+
+    //undefined, null,0 or '' are COERCED to false
+    //Anything else is COERCED to true.
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
     //3. Check if the player own the game.
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= winningScore) {
       document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-      document.querySelector(".dice").style.display = "none";
+      document.getElementById("dice-1").style.display = "none";
+      document.getElementById("dice-2").style.display = "none";
       document
         .querySelector(".player-" + activePlayer + "-panel")
         .classList.add("winner");
@@ -100,16 +128,18 @@ function nextPlayer() {
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
 
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
+  document.querySelector(".btn-new").addEventListener("click", init);
 }
-document.querySelector(".btn-new").addEventListener("click", init);
 
 function init() {
   scores = [0, 0];
   activePlayer = 0;
   roundScore = 0;
   gamePlaying = true;
-  document.querySelector(".dice").style.display = "none";
+  document.getElementById("dice-1").style.display = "none";
+  document.getElementById("dice-2").style.display = "none";
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
   document.getElementById("current-0").textContent = "0";
